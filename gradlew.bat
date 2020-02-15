b0b942371ff6cdaeb21aa79faaacbae13df604a1
Mushegh11 <a@mail.ru>
2020-02-16 00:00:42 +0400

    First

---
+++ gradlew.bat
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<root>
+  <item name="java.awt.AWTKeyStroke AWTKeyStroke(char, int, int, boolean) 2">
+    <annotation name="org.intellij.lang.annotations.MagicConstant">
+      <val name="flagsFromClass" val="java.awt.event.InputEvent.class" />
+    </annotation>
+  </item>
+  <item name="java.awt.AWTKeyStroke int getModifiers()">
+    <annotation name="org.intellij.lang.annotations.MagicConstant">
+      <val name="flagsFromClass" val="java.awt.event.InputEvent.class" />
+    </annotation>
+  </item>
+  <item name="java.awt.AWTKeyStroke java.awt.AWTKeyStroke getAWTKeyStroke(int, int) 1">
+    <annotation name="org.intellij.lang.annotations.MagicConstant">
+      <val name="flagsFromClass" val="java.awt.event.InputEvent.class" />
+    </annotation>
+  </item>
+  <item name="java.awt.AWTKeyStroke java.awt.AWTKeyStroke getAWTKeyStroke(int, int, boolean) 1">
+    <annotation name="org.intellij.lang.annotations.MagicConstant">
+      <val name="flagsFromClass" val="java.awt.event.InputEvent.class" />
+    </annotation>
+  </item>
+  <item name="java.awt.AWTKeyStroke java.awt.AWTKeyStroke getAWTKeyStroke(java.lang.Character, int) 1">
+    <annotation name="org.intellij.lang.annotations.MagicConstant">
+      <val name="flagsFromClass" val="java.awt.event.InputEvent.class" />
+    </annotation>
+  </item>
+  <item name="java.awt.BasicStroke BasicStroke(float, int, int) 1">
+    <annotation name="org.intellij.lang.annotations.MagicConstant">
+      <val name="intValues" val="{java.awt.BasicStroke.CAP_BUTT, java.awt.BasicStroke.CAP_ROUND, java.awt.BasicStroke.CAP_SQUARE}" />
+    </annotation>
+  </item>
+  <item name="java.awt.BasicStroke BasicStroke(float, int, int) 2">
+    <annotation name="org.intellij.lang.annotations.MagicConstant">
+      <val name="intValues" val="{java.awt.BasicStroke.JOIN_BEVEL, java.awt.BasicStroke.JOIN_MITER, java.awt.BasicStroke.JOIN_ROUND}" />
+    </annotation>
+  </item>
+  <item name="java.awt.BasicStroke BasicStroke(float, int, int, float) 1">
+    <annotation name="org.intellij.lang.annotations.MagicConstant">
+      <val name="intValues" val="{java.awt.BasicStroke.CAP_BUTT, java.awt.BasicStroke.CAP_ROUND, java.awt.BasicStroke.CAP_SQUARE}" />
+    </annotation>
+  </item>
+  <item name="java.awt.BasicStroke B
